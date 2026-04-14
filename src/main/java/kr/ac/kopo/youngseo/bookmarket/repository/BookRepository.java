7846d3ab90b10b0aa34366a38f6020bc4c19a151
JeongYoungseo <wjddudtjjys@gmail.com>
2026-04-14 11:29:39 +0900

    도서목록

--- src/main/java/kr/ac/kopo/youngseo/bookmarket/repository/BookRepository.java
+++ src/main/java/kr/ac/kopo/youngseo/bookmarket/repository/BookRepository.java
@@ -3,7 +3,12 @@ package kr.ac.kopo.youngseo.bookmarket.repository;
 import kr.ac.kopo.youngseo.bookmarket.domain.Book;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public interface BookRepository {
     List<Book> getAllBookList();
+    Book getBookById(String bookId);
+    List<Book> getBookListByCategory(String category);
+    Set<Book> getBookListByFilter(Map<String, List<String>> filter);
 }
